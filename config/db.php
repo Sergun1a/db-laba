f9d40671faeb1e258a9744a05ba890ca427c5484
Sergey Alaev <webest468584>
2020-11-10 20:51:49 +0300

    Updated db config and composer files.

--- config/db.php
+++ config/db.php
@@ -1,14 +1,29 @@
 <?php
+$server   = 'eu-cdbr-west-03.cleardb.net:3306';
+$username = 'b20409eee3af56';
+$password = 'e9bda834';
+$db       = 'heroku_e130fe6d416e3df';
 
+/* Heroku config */
+return [
+    'class'    => 'yii\db\Connection',
+    'dsn'      => 'mysql:host=localhost;dbname=' . $db,
+    'username' => $username,
+    'password' => $password,
+    'charset'  => 'utf8',
+];
+
+
+
+/* Local config
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=dblaba',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
+*/
+
+
+
