25b2ebc154cc4dc63f255641668b7dd5ba45bc3f
Сергей Алаев <alaev.serezha@yandex.ru>
2022-04-14 13:06:26 +0300

    Updated db data for working with heroku

--- config/db.php
+++ config/db.php
@@ -1,8 +1,9 @@
 <?php
-$server   = 'eu-cdbr-west-03.cleardb.net:3306';
-$username = 'b20409eee3af56';
-$password = 'e9bda834';
-$db       = 'heroku_e130fe6d416e3df';
+$server   = 'eu-cdbr-west-02.cleardb.net:3306';
+$username = 'be8c5359d4d40e';
+$password = '609c68a3';
+$db       = 'heroku_360d2bca5a42a21';
+
 
 /* Heroku config */
 return [
@@ -15,15 +16,15 @@ return [
 
 
 
-/* Local config
-return [
+/* Local config */
+/*return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=dblaba',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
-];
-*/
+];*/
+
 
 
 
